90b3833bbb81a968d73cdee4659b8f1eafc4f043
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2020-10-22 13:12:15 -0400

    Update task-view.component.ts

--- taskApp/src/app/pages/task-view/task-view.component.ts
+++ taskApp/src/app/pages/task-view/task-view.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-task-view',
-  templateUrl: './task-view.component.html',
+  templateUrl: './task-view.component.html', 
   styleUrls: ['./task-view.component.css']
 })
 export class TaskViewComponent{
@@ -10,29 +11,7 @@ export class TaskViewComponent{
   to add a division */
   public items = []; 
 
-  /* A two-way binding performed which 
-    pushes text on division */
-  public newTask; 
-
   public newCat;
-
-
-  /* When input is empty, it will 
-    not create a new division */
-  public addToList() { 
-      if (this.newTask == '') { 
-      } 
-      else { 
-          this.items.push(this.newTask); 
-          this.newTask = ''; 
-      } 
-  } 
-
-  /* This function takes to input the 
-    task, that has to be deleted*/
-  public deleteTask(index) { 
-      this.items.splice(index, 1); 
-  }
 
   public addCat(){
     if(this.newCat==' '){
