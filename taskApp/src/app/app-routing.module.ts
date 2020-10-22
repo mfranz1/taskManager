be09e1c89d577cabccde9580a22aeb7a8098277c
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2020-10-22 13:11:59 -0400

    Update app-routing.module.ts

--- taskApp/src/app/app-routing.module.ts
+++ taskApp/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewCatComponent } from './pages/new-cat/new-cat.component';
+import { ListTasksComponent } from './pages/list-tasks/list-tasks.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/cats', pathMatch:'full'},
   { path: 'new-cat', component: NewCatComponent},
   { path: 'cats', component: TaskViewComponent },
+  { path: 'list-tasks',component: ListTasksComponent}
 ];
 
 @NgModule({
